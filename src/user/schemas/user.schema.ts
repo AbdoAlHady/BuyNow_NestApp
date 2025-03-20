ba98a83bce9039f83df3fa4193eada89e2336f3a
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-03-21 01:36:28 +0200

    add user schema with validation and properties for user management

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,70 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({
+  timestamps: true,
+  versionKey: false,
+})
+export class User {
+  @Prop({
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 30,
+  })
+  name: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 20,
+  })
+  password: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    enum: ['user', 'admin', 'manager'],
+    default: 'user',
+  })
+  role: string;
+
+  @Prop({ type: String })
+  avatar: string;
+
+  @Prop({ type: Number })
+  age: number;
+
+  @Prop({ type: Number, minlength: 11, maxlength: 11 })
+  phoneNumber: number;
+
+  @Prop({ type: String })
+  address: string;
+
+  @Prop({ type: Boolean, default: fail })
+  active: boolean;
+
+  @Prop({ type: String })
+  verificationCode: string;
+
+  @Prop({ type: Date })
+  verificationCodeExpires: Date;
+
+  @Prop({ type: String, enum:['male','female'] })
+  gender: string;
+
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
