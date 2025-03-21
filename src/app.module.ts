1cddc5641102edb94443dc159f052847913e5d1f
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-03-21 04:27:09 +0200

    integrate nestjs-i18n for internationalization support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,38 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import {
+  AcceptLanguageResolver,
+  HeaderResolver,
+  I18nModule,
+  QueryResolver,
+} from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+        new HeaderResolver(['x-lang']),
+      ],
+    }),
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env.development' }),
     MongooseModule.forRootAsync({
-      useFactory: (config: ConfigService) =>{
-        return{
+      useFactory: (config: ConfigService) => {
+        return {
           uri: config.get('DATABASE_URL'),
-        }
+        };
       },
       inject: [ConfigService],
     }),
-    UserModule
+    UserModule,
   ],
   controllers: [],
   providers: [],
