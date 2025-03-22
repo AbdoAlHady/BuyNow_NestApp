4d5e8b145c3d97abc2fea6feef3b1552fef5a104
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-03-22 14:14:35 +0200

    replace generic error with BadRequestException in MongoIdValidationPipe

--- src/utils/pipes/mongo-id-validation.pipe.ts
+++ src/utils/pipes/mongo-id-validation.pipe.ts
@@ -1,11 +1,11 @@
-import {Injectable, PipeTransform } from '@nestjs/common';
+import {BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 import { Types } from 'mongoose';
 
 @Injectable()
 export class MongoIdValidationPipe implements PipeTransform {
   transform(value: string ): string  {
     if (!Types.ObjectId.isValid(value)) {
-      throw new Error('Invalid MongoDB ObjectId');
+      throw new BadRequestException('Invalid MongoDB ObjectId');
     }
     return value;
   }
