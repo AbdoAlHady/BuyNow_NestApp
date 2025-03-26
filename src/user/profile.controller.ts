3e602355e2203c68161a6af618b1c0f0a2c91900
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-03-27 01:46:31 +0200

    implement profile management; add profile update DTO and controller for user profile operations

---
+++ src/user/profile.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Delete, Get, Patch, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { JwtPayloadType } from 'src/utils/types';
+import { ProfileUpdateUserDto } from './dto/profile-update-user.dto';
+
+@Controller('profile')
+export class ProfileController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  @UseGuards(AuthGuard)
+  async getLoggedUserData(@CurrentUser() payload: JwtPayloadType) {
+    return this.userService.getSpecialUser(payload.id);
+  }
+
+  @Patch()
+  @UseGuards(AuthGuard)
+  async updateLoggedUser(@Body()profileUpdateUserDto:ProfileUpdateUserDto,@CurrentUser() payload: JwtPayloadType) {
+    return this.userService.update(payload.id, profileUpdateUserDto);
+  }
+
+  @Delete()
+  @UseGuards(AuthGuard)
+  async deleteLoggedUser(@CurrentUser() payload: JwtPayloadType) {
+    return this.userService.remove(payload.id);
+  }
+  
+}
