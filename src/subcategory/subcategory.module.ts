991ea8ccfa973fb87feb719815b0e55498a73776
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-03-28 03:12:48 +0200

    add Mongoose integration for subcategory module; include schema and import configuration

--- src/subcategory/subcategory.module.ts
+++ src/subcategory/subcategory.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SubcategoryService } from './subcategory.service';
 import { SubcategoryController } from './subcategory.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SubCategory, SubCategorySchema } from './schema/subcategory.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: SubCategory.name, schema: SubCategorySchema },
+    ]),
+  ],
   controllers: [SubcategoryController],
   providers: [SubcategoryService],
 })
