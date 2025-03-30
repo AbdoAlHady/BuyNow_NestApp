ccedf13d3b4ec508a7ad0db5d664118d550e055d
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-03-30 12:30:44 +0200

    remove brand service test file and enhance create brand DTO with validation rules

--- src/brand/dto/create-brand.dto.ts
+++ src/brand/dto/create-brand.dto.ts
@@ -1,1 +1,11 @@
-export class CreateBrandDto {}
+import { IsOptional, IsUrl, MaxLength, MinLength } from 'class-validator';
+
+export class CreateBrandDto {
+  @MinLength(3, { message: 'Brand name must be at least 3 characters' })
+  @MaxLength(100, { message: 'Brand name must be 100 characters' })
+  name: string;
+
+  @IsOptional()
+  @IsUrl({}, { message: 'Invalid URL' })
+  image: string;
+}
