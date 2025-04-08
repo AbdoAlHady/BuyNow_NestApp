8330198e131c877f5ecd81686e639d44c53b1b51
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-04-08 17:10:54 +0200

    add coupon module imports for Mongoose and UserModule; configure schema for coupon

--- src/coupon/coupon.module.ts
+++ src/coupon/coupon.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CouponService } from './coupon.service';
 import { CouponController } from './coupon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Coupon, CouponSchema } from './schemas/coupon.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [CouponController],
   providers: [CouponService],
+  imports: [
+    MongooseModule.forFeature([{
+      schema:CouponSchema,
+      name: Coupon.name,
+    }]),
+    UserModule,
+  ],
+
 })
 export class CouponModule {}
