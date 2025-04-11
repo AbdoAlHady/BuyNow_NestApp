11c78f45a7eec36b956a90b66b920f061f7ef906
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-04-11 03:52:01 +0200

    feat(tax): add Mongoose integration for Tax schema and User module

--- src/tax/tax.module.ts
+++ src/tax/tax.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TaxService } from './tax.service';
 import { TaxController } from './tax.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Tax, TaxSchema } from './schemas/tax.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [TaxController],
   providers: [TaxService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Tax.name,
+        schema: TaxSchema,
+      },
+    ]),
+    UserModule
+  ],
 })
 export class TaxModule {}
