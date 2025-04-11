902c3abcb5760c365419ab87e839eb51a416a017
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-04-11 04:07:17 +0200

    feat(tax): implement createOrUpdateTax and resetTax methods in TaxService

--- src/tax/tax.service.ts
+++ src/tax/tax.service.ts
@@ -1,26 +1,56 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaxDto } from './dto/create-tax.dto';
-import { UpdateTaxDto } from './dto/update-tax.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Tax } from './schemas/tax.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class TaxService {
-  create(createTaxDto: CreateTaxDto) {
-    return 'This action adds a new tax';
+  constructor(@InjectModel(Tax.name) private readonly taxModel: Model<Tax>) {}
+  /**
+   * Create Or Update a tax price and shipping price
+   * @param createTaxDto - The tax price and shipping price
+   * @returns  The created tax price and shipping price
+   * @access Admin
+   */
+  public async createOrUpdateTax(createTaxDto: CreateTaxDto) {
+    const tax = await this.taxModel.findOneAndUpdate({}, createTaxDto, {
+      new: true,
+      upsert: true,
+    });
+    return {
+      status: 'success',
+      data: tax,
+    };
   }
 
-  findAll() {
-    return `This action returns all tax`;
+  /**
+   * Get tax price and shipping price
+   * @returns  The tax price and shipping price
+   * @access Admin
+   */
+  public async GetTax() {
+    return {
+      status: 'success',
+      data: await this.taxModel.findOne({}),
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} tax`;
-  }
-
-  update(id: number, updateTaxDto: UpdateTaxDto) {
-    return `This action updates a #${id} tax`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} tax`;
+  /**
+   * reset tax price and shipping price
+   * @returns  The updated tax price and shipping price
+   * @access Admin
+   */
+  public async resetTax() {
+    const tax = await this.taxModel.findOneAndUpdate(
+      {},
+      { taxPrice: 0, shippingPrice: 0 },
+      { new: true },
+    );
+    return {
+      status: 'success',
+      data: tax,
+      message: 'Tax price and shipping price reset successfully',
+    };
   }
 }
