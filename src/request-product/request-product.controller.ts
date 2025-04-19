8ce361c3afbbfe0ceedec455c614dd36125a80ec
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-04-19 03:24:44 +0200

    feat(request-product): enhance controller and service with user authentication and CRUD operations

--- src/request-product/request-product.controller.ts
+++ src/request-product/request-product.controller.ts
@@ -1,34 +1,72 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Query,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
 import { RequestProductService } from './request-product.service';
 import { CreateRequestProductDto } from './dto/create-request-product.dto';
 import { UpdateRequestProductDto } from './dto/update-request-product.dto';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { AuthRolesGuard } from 'src/auth/guard/auth-roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { CurrntUser } from 'src/utils/decorators/current_user_decorators';
+import { JwtPayloadType } from 'src/utils/types';
+import { MongoIdValidationPipe } from 'src/utils/pipes/mongo-id-validation.pipe';
 
+@UseGuards(AuthGuard, AuthRolesGuard)
 @Controller('request-product')
 export class RequestProductController {
   constructor(private readonly requestProductService: RequestProductService) {}
 
   @Post()
-  create(@Body() createRequestProductDto: CreateRequestProductDto) {
-    return this.requestProductService.create(createRequestProductDto);
+  @Roles(['user'])
+  create(
+    @Body() createRequestProductDto: CreateRequestProductDto,
+    @CurrntUser() payload: JwtPayloadType,
+  ) {
+    console.log(payload);
+    return this.requestProductService.create(
+      createRequestProductDto,
+      payload.id,
+    );
   }
 
   @Get()
-  findAll() {
-    return this.requestProductService.findAll();
+  @Roles(['admin'])
+  getAllRequestedProducts(@Query() query: any) {
+    return this.requestProductService.getAllRequestProducts(query);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.requestProductService.findOne(+id);
+  @Roles(['admin', 'user'])
+  getSpecificRequestedProduct(@Param('id', MongoIdValidationPipe) id: string) {
+    return this.requestProductService.getSpecificRequestProduct(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRequestProductDto: UpdateRequestProductDto) {
-    return this.requestProductService.update(+id, updateRequestProductDto);
+  @Roles(['user'])
+  update(
+    @Param('id', MongoIdValidationPipe) id: string,
+    @Body() updateRequestProductDto: UpdateRequestProductDto,
+  ) {
+    return this.requestProductService.updateRequestProduct(
+      id,
+      updateRequestProductDto,
+    );
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.requestProductService.remove(+id);
+  @Roles(['user'])
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id', MongoIdValidationPipe) id: string) {
+    await this.requestProductService.deleteRequestProduct(id);
   }
 }
--- src/request-product/request-product.service.ts
+++ src/request-product/request-product.service.ts
@@ -1,26 +1,75 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRequestProductDto } from './dto/create-request-product.dto';
 import { UpdateRequestProductDto } from './dto/update-request-product.dto';
+import { BaseService } from 'src/utils/services/base.service';
+import { RequestProduct } from './schemas/request-product.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class RequestProductService {
-  create(createRequestProductDto: CreateRequestProductDto) {
-    return 'This action adds a new requestProduct';
+export class RequestProductService extends BaseService<RequestProduct> {
+  constructor(
+    @InjectModel(RequestProduct.name)
+    private readonly requestModel: Model<RequestProduct>,
+  ) {
+    super(requestModel);
   }
 
-  findAll() {
-    return `This action returns all requestProduct`;
+  /**
+   * Create a new request product
+   * @param createRequestProductDto - Request product data to create
+   * @param userId - User ID to associate with the request product
+   * @returns  New Request Product From Database
+   * @access User
+   */
+  create(createRequestProductDto: CreateRequestProductDto, userId: string) {
+    return this.createOne({
+      ...createRequestProductDto,
+      user: userId,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} requestProduct`;
+  /**
+   * Get all request products
+   * @param query - Query to find request products
+   * @returns  All request products from database
+   * @access Admin
+   */
+  public async getAllRequestProducts(query: any) {
+    return await this.findAll(query);
   }
 
-  update(id: number, updateRequestProductDto: UpdateRequestProductDto) {
-    return `This action updates a #${id} requestProduct`;
+  /**
+   * Get a request product by id
+   * @param id - Request product ID to find
+   * @returns  Request product from database
+   * @access admin, user
+   */
+  public async getSpecificRequestProduct(id: string) {
+    return await this.findOne(id);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} requestProduct`;
+  /**
+   *  Update a request product
+   * @param id  - Request product ID to find
+   * @param updateRequestProductDto  - Request product data to update
+   * @returns The updated request product from database
+   * @access user
+   */
+  public async updateRequestProduct(
+    id: string,
+    updateRequestProductDto: UpdateRequestProductDto,
+  ) {
+    return await this.updateOne(id, updateRequestProductDto);
+  }
+
+  /**
+   *  Delete a request product
+   * @param id  - Request product ID to find
+   * @returns The deleted request product from database
+   * @access  user
+   */
+  public async deleteRequestProduct(id: string) {
+    await this.deleteOne(id);
   }
 }
