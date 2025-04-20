b933947ce7e7ad39210811c17f187b9be6c1901a
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-04-20 14:30:49 +0200

    remove unused test files for ProductController and ProductService

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './schemas/product.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [ProductController],
   providers: [ProductService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name:Product.name,
+        schema:ProductSchema
+      }
+    ]),
+    UserModule
+  ],
 })
 export class ProductModule {}
