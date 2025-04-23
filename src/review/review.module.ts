2e1d73bb2384099dfc02a0de8cf103d3bf8c160e
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-04-24 00:30:23 +0200

    refactor(review): remove unused test files for controller and service

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Review, ReviewSchema } from './schemas/review.schema';
+import { UserModule } from 'src/user/user.module';
+import { ProductModule } from 'src/product/product.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name:Review.name,
+        schema:ReviewSchema
+      }
+    ]),
+
+    UserModule,
+    ProductModule,
+  ],
   controllers: [ReviewController],
   providers: [ReviewService],
 })
